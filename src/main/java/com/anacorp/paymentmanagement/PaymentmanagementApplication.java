c7d8f815a95c3199339d1ccf6ec21721e24724a8
anacristinavaz <anacrisvaz@hotmail.com>
2025-03-08 18:56:58 -0300

    first commit

---
+++ src/main/java/com/anacorp/paymentmanagement/PaymentmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.anacorp.paymentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentmanagementApplication.class, args);
+	}
+
+}
